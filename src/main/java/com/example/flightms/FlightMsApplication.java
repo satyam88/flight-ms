2f6f39f085045987c6b5e85886780e548daf5f6f
“Satyam <satyam@gmail.com>
2023-12-17 15:41:07 +0530

    codeRefactor

---
+++ src/main/java/com/example/flightms/FlightMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightMsApplication.class, args);
+	}
+
+}
